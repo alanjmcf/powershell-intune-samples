914449b4641f6faa50399cd31d738bcdd6f2f3ef
Alan McFarlane <alanjmcf@outlook.com>
2022-12-13 15:46:50 +0000

    Spark into manual connect

--- IntuneLuma1.ps1
+++ IntuneLuma1.ps1
@@ -5,6 +5,10 @@ $onMsftPart = $null
 $tenant = '9bb6d34f-737b-4616-b809-c4ff85b21da3'
 $onMsftPart = 'bridginggap'
 #
+# Spark
+$tenant = "23d7aca0-926d-4e63-8a82-e3874896f36f"
+$onMsftPart = 'thespark'
+#
 if (-not $onMsftPart) {
     $onMsftPart = $tenant.Substring(0, $tenant.IndexOf('.'))
 }
